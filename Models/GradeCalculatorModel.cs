97e7b74620d85b3ec532c7468f5da30c8c6c546d
nstaplet <nstaplet7@gmail.com>
2021-01-27 23:33:52 -0700

    Assignment 2 changes

---
+++ Models/GradeCalculatorModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace NatesFirstASPNETWebApp.Models
+{
+    public class GradeCalculatorModel
+    {
+        // one line getter and setter and a range restriction on the user input
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public String assignments { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public String groupproject { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public String quizzes { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public String exams { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public String intex { get; set; }
+
+    }
+}
---
+++ wwwroot/js/gradecalculator.js
@@ -0,0 +1,58 @@
+﻿$("#submitbutton").click(function () {
+    var assignments = parseInt($("#assignments").val())
+    var groupproject = parseInt($("#groupproject").val())
+    var quizzes = parseInt($("#quizzes").val())
+    var exams = parseInt($("#exams").val())
+    var intex = parseInt($("#intex").val())
+
+    var finalgrade = assignments*.5 + groupproject*.1 + quizzes*.1 + exams*.2 + intex*.1
+
+    if (finalgrade >= 94) {
+        alert("your final grade was " + finalgrade + "% which is an A.")
+    }
+
+    else if (finalgrade >= 90) {
+        alert("your final grade was " + finalgrade + "% which is an A-.")
+    }
+
+    else if (finalgrade >= 87) {
+        alert("your final grade was " + finalgrade + "% which is a B+.")
+    }
+
+    else if (finalgrade >= 84) {
+        alert("your final grade was " + finalgrade + "% which is a B.")
+    }
+
+    else if (finalgrade >= 80) {
+        alert("your final grade was " + finalgrade + "% which is a B-.")
+    }
+
+    else if (finalgrade >= 77) {
+        alert("your final grade was " + finalgrade + "% which is a C+.")
+    }
+
+    else if (finalgrade >= 74) {
+        alert("your final grade was " + finalgrade + "% which is a C.")
+    }
+
+    else if (finalgrade >= 70) {
+        alert("your final grade was " + finalgrade + "% which is a C-.")
+    }
+
+    else if (finalgrade >= 67) {
+        alert("your final grade was " + finalgrade + "% which is a D+.")
+    }
+
+    else if (finalgrade >= 64) {
+        alert("your final grade was " + finalgrade + "% which is a D.")
+    }
+
+    else if (finalgrade >= 60) {
+        alert("your final grade was " + finalgrade + "% which is a D-.")
+    }
+
+    else {
+        alert("your final grade was " + finalgrade + "% which is an E.")
+    }
+
+});
